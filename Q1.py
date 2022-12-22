cc10536c9396f41720cd638e2067920de05e10e5
Akshat39 <121255464+Akshat39@users.noreply.github.com>
2022-12-22 23:58:45 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,6 @@
+first = int(input("Enter the First Number : "))
+second = int(input("Enter the Second Number : "))
+third = int(input("Enter the Third Number : "))
+avg = (first+second+third)/3
+print("The Average of the Three Numbers is: ", avg)
+      
---
+++ Q5.py
@@ -0,0 +1,6 @@
+from math import *
+
+i = 0
+while i <= 345:
+    print(i, '---', round(sin(i), 4), round(cos(i), 4))
+    i += 15
